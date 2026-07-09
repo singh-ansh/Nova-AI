a722739063800ce6b11e3ddb5dc800b072f22e18
Ansh <ayush5798ji@gmail.com>
2026-07-09 12:08:41 +0530

    added render url

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://nova-ai-5opr.onrender.com/api/auth",
 });
 
 export const registerUser = async (userData) => {
