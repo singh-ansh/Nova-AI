115b4d2245a51895150fde658df0263712d0b7be
Ansh <ayush5798ji@gmail.com>
2026-07-13 08:20:25 +0530

    file upload error handled

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,10 +1,19 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+// Absolute uploads path
+const uploadDir = path.join(__dirname, "../uploads");
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Storage
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename(req, file, cb) {
